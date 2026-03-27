4f24e84f6822b4a4654de50d5629289cbdfb7923
Joacco <joaquingodoy2407@gmail.com>
2026-03-27 12:13:58 -0300

    test: add smoke test

---
+++ tests/app_test.rs
@@ -0,0 +1,13 @@
+use wasm_bindgen_test::*;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn app_mounts_without_panic() {
+    use leptos::prelude::*;
+    use trinidad_margni_portfolio::App;
+
+    mount_to_body(|| {
+        view! { <App /> }
+    });
+}
